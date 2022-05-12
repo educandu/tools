59bf1ff6259255fd9c17baa7838fd1c8055ba913
Andreas Helmberger <public@ahelmberger.de>
2022-05-12 15:58:18 +0200

    NOTICKET: Update project to more modern node version and update packages

--- unique-id.js
+++ unique-id.js
@@ -1,5 +1,5 @@
-const uuidv4 = require('uuid/v4');
 const anyBase = require('any-base');
+const { v4: uuidv4 } = require('uuid');
 
 const flickrBase58 = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
 const hexToFlickrBase58 = anyBase(anyBase.HEX, flickrBase58);
